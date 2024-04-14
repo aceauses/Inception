5ca7aa9e45311a1c6cfa965aa2c948ebb8ba6f0a
aceauses <aceauses@student.42heilbronn.de>
2024-04-14 17:37:48 +0200

    Update/Fix
    Make wordpress and nginx work together, add more .env variables for easier modifyign accesibility for config of the inception and the wordpress admin and author user. also add the domain variable so you can change easily the domain,
    It still need SSL certificates in the nginx.

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Give ownership of the WordPress files to the Nginx user
 chown -R www-data:www-data /var/www/html
-
+sed -i "s/XXXXXXX/$DOMAIN_NAME/g" /etc/nginx/sites-available/default
 nginx -g 'daemon off;'
